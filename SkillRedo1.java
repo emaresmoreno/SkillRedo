55bec44bc2430af225c26834771aec04bedf61e1
emaresmoreno <emaresmoreno@ucsd.edu>
2022-05-11 21:12:29 -0700

    caused an error

--- SkillRedo1.java
+++ SkillRedo1.java
@@ -1,2 +1,5 @@
-public class SkillRedo.java{
+public class SkillRedo1{
+    public static int divide(int a, int b){
+        return a % b;
+    }
 }
---
+++ SkillRedoTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillRedoTest {
+    @Test
+    public void test(){
+        assertEquals("We expect 4 to be divided by 2", 
+        2, SkillRedo1.divide(4,2));
+
+    }
+}
